df84be3c87463a7fd23cd56be3d7923108c9951e
1103-Chumpol-James <257372671+1103-Chumpol-James@users.noreply.github.com>
2026-04-20 16:29:58 -0700

    Initial commit

---
+++ twordle.c
@@ -0,0 +1,128 @@
+// James Chumpol
+// CS135, Section 1001/1103
+
+#include <stdio.h>
+#define WORDFILE "mystery.txt"
+#define TRUE 1
+#define FALSE 0
+#define ERR 1
+#define ROW_SIZE 2
+#define WORD_SIZE 5
+#define COL_SIZE 10
+#define CHANGE_CASE 32
+
+int foundFile(FILE *file)
+{
+  if (file)
+  {
+    return TRUE;
+  }
+  printf("Unable to open file.\n");
+  return FALSE;
+}
+
+// Test function to quickly print 2D arrays.
+void printArr(char arr[ROW_SIZE][COL_SIZE], int row)
+{
+  for (int i = 0; i < WORD_SIZE; i++)
+  {
+    printf("%c", arr[row][i]);
+  }
+  printf("\n");
+}
+
+void getGoalWord(FILE *file, char word[WORD_SIZE])
+{
+  char inputChar;
+  int columnPos;
+  for (columnPos = 0; fscanf(file, "%c", &inputChar) == 1; columnPos++)
+  {
+    if (inputChar == '\0')
+    {
+      return;
+    }
+    word[columnPos] = inputChar;
+  }
+}
+
+void compareSubString(char wordGoal[WORD_SIZE], char wordGuess[ROW_SIZE][COL_SIZE])
+{
+  int columnPos;
+  int goal = 0;
+  int hint = 1;
+  for (columnPos = 0; columnPos < WORD_SIZE; columnPos++)
+  {
+    char letterGoal = wordGoal[columnPos];
+    char letterGuess = wordGuess[goal][columnPos];
+    if (letterGoal == letterGuess)
+    {
+      wordGuess[goal][columnPos] -= CHANGE_CASE;
+    }
+    else
+    {
+      for (int i = 0; i < WORD_SIZE; i++)
+      {
+        // TODO: fix hint substring
+        char letterGoal = wordGoal[i];
+        if (letterGuess == letterGoal)
+        {
+          wordGuess[hint][columnPos] = '^';
+          i = WORD_SIZE;
+        }
+        else
+        {
+          wordGuess[hint][columnPos] = 'x';
+        }
+      }
+    }
+  }
+
+  printf("%s", wordGuess);
+  if (wordGuess[hint])
+  {
+    printf("\n%s\n", wordGuess[hint]);
+  }
+}
+
+void toLowerCase(char wordGuess[ROW_SIZE][COL_SIZE])
+{
+  int row = 0;
+  for (int i = 0; i < WORD_SIZE; i++)
+  {
+    if (wordGuess[row][i] == '\0') return;
+    if (wordGuess[row][i] >= 'A' && wordGuess[row][i] <= 'Z')
+    {
+      wordGuess[row][i] += CHANGE_CASE;
+    }
+  }
+  printf("new lowercase word: %s\n", wordGuess[row]);
+}
+
+void startGame(char wordGoal[WORD_SIZE])
+{
+  int attempt = 1;
+  printf("GUESS %d: ", attempt);
+
+  char wordGuess[ROW_SIZE][COL_SIZE] = {0};
+  scanf("%s", &wordGuess);
+
+  toLowerCase(wordGuess);
+  compareSubString(wordGoal,wordGuess);
+}
+
+int main()
+{
+  FILE *inputFile = fopen(WORDFILE, "r");
+  if (!foundFile(inputFile))
+  {
+    return ERR;
+  }
+
+  char wordGoal[WORD_SIZE];
+  getGoalWord(inputFile, wordGoal);
+  fclose(inputFile);
+
+  printf("Welcome to Twordle!\n");
+  printf("Please enter your first guess that is 5 letters long.\n");
+  startGame(wordGoal);
+}
